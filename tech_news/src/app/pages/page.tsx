ba5a30cfc959def25f4cbe8de07a408bb5c3035f
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-16 20:37:36 -0300

    Importando o componente carousel para ser implementado na pagina

--- tech_news/src/app/pages/page.tsx
+++ tech_news/src/app/pages/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+import CarouselNews from "../components/CarouselNews";
 
 export default function Home() {
   return (
-    <div>
-      <NavBar></NavBar>
-      <h1>Esta eh a home page</h1>
-    </div>
+    <main className="mx-auto">
+        <CarouselNews />
+        <div className="mt-3 p-4 mx-auto">
+          <h1>Esta eh a home page</h1>
+        </div>
+    </main>
   );
 }
