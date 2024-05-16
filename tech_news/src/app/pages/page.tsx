ef282db3f14f0da326fd0a23b8b2f4208bb093a6
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-16 07:34:54 -0300

    chamando o componente NavBar para ser utilizado na home page

--- tech_news/src/app/pages/page.tsx
+++ tech_news/src/app/pages/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import NavBar from "../components/NavBar"
 
 export default function Home() {
   return (
-    <h1>Esta eh a home page</h1>
+    <div>
+      <NavBar></NavBar>
+      <h1>Esta eh a home page</h1>
+    </div>
   );
 }
