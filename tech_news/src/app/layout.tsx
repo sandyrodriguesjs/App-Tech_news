bba8eea0957d6bbae3151054f1de874fd81643c3
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-17 15:54:10 -0300

    Movendo a logica de alternância do DarkMode para o arquivo layout e adicionando os props que serão chamados no Componente NavBar

--- tech_news/src/app/layout.tsx
+++ tech_news/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer"
 import NavBar from "./components/NavBar"
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,23 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <NavBar />
+      <body className={`${inter.className} ${isDarkMode ? "bg-gray-900" : "bg-white"}`}>
+        <NavBar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
         {children}
         <footer className='bg-gray-600 text-white py-4 mt-10 my-auto'>
           <Footer />
