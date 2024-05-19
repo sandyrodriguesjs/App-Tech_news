03a47ec17d676e4793cda31075339392581bff1e
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-19 10:42:42 -0300

    Adicionando a seção de destaque/introdução  da página

--- tech_news/src/app/pages/page.tsx
+++ tech_news/src/app/pages/page.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
-import NavBar from "../components/NavBar"
-import Footer from "../components/Footer"
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import CarouselNews from "../components/CarouselNews";
 
 export default function Home() {
   return (
-    <main className="mx-auto">
-        <CarouselNews />
-        <div className="mt-3 p-4 mx-auto">
-          <h1>Esta eh a home page</h1>
+    <main className="container mx-auto px-4 py-8">
+      <section className="hero mb-8">
+        <div className="flex flex-col md:flex-row md:items-center justify-between">
+          <div className="flex-grow">
+            <h1 className="text-3xl font-bold leading-tight mb-3">Bem Vindo ao Tech News Hub!</h1>
+            <p className="text-lg text-gray-700">
+              Mantenha-se informado sobre os últimos avanços e tendências que moldam o mundo da tecnologia.
+            </p>
+          </div>
         </div>
+      </section>
+
+      <CarouselNews /> {/* Include the news carousel component */}
     </main>
   );
 }
