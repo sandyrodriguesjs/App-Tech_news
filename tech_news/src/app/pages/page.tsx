1efb9bd9fd93bf35332d8c2292ce601cf6908352
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-21 11:59:00 -0300

    Criando a seção de Artigos em Destaque na página

--- tech_news/src/app/pages/page.tsx
+++ tech_news/src/app/pages/page.tsx
@@ -18,6 +18,32 @@ export default function Home() {
       </section>
 
       <CarouselNews /> {/* Include the news carousel component */}
+
+      <section className="featured-articles mb-8 mt-3">
+        <h2 className="text-2xl font-semibold mb-4">Artigos em Destaque</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {/* Add placeholders or links to actual articles here */}
+          <article className="rounded-lg shadow-md overflow-hidden">
+            <div className="p-4">
+              <h3 className="text-lg font-medium mb-2">O surgimento das IA's generativas</h3>
+              <p className="text-base text-gray-700 line-clamp-3">
+                Big Techs investem pesado em criação dos chatbot's com Inteligência Artificial
+              </p>
+              <a href="#" className="text-blue-500 hover:text-blue-700">Saiba Mais</a>
+            </div>
+          </article>
+          <article className="rounded-lg shadow-md overflow-hidden">
+            <div className="p-4">
+              <h3 className="text-lg font-medium mb-2">Controle das máquinas com a mente</h3>
+              <p className="text-base text-gray-700 line-clamp-3">
+                Neuralink faz 1° implante de chip cerebral em humano
+              </p>
+              <a href="#" className="text-blue-500 hover:text-blue-700">Saiba Mais</a>
+            </div>
+          </article>
+        </div>
+      </section>
+
     </main>
   );
 }
