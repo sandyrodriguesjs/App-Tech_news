393aaa71202957bea69c83c32050ce5c31d2f649
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-21 14:52:19 -0300

    Implementando as imagens que foram adicionadas no projeto em seus respectivos anúncios

--- tech_news/src/app/pages/page.tsx
+++ tech_news/src/app/pages/page.tsx
@@ -47,6 +47,52 @@ export default function Home() {
         </div>
       </section>
 
+      <section className="latest-news mb-8 mt-3">
+        <h2 className="text-2xl font-semibold mb-4">Últimas Notícias</h2>
+        <div className="flex flex-col space-y-4">
+          {/* Add placeholders or links to actual news here */}
+          <article className="flex rounded-lg shadow-md overflow-hidden">
+            <img src="/spotify.avif" alt="Descrição da imagem 1" className="w-1/3 object-cover"/>
+            <div className="p-4">
+              <h3 className="text-lg font-medium mb-2">10 anos do Spotify no Brasil: 5 curiosidades sobre o streaming de música</h3>
+              <p className="text-base text-gray-700 line-clamp-3">
+                Spotify completa 10 anos no Brasil, nesta terça-feira (21). App é a maior plataforma de streaming de música do mundo, com mais de 600 milhões de usuários ativos; confira
+              </p>
+              <a href="#" className="text-blue-500 hover:text-blue-700">Leia Mais</a>
+            </div>
+          </article>
+          <article className="flex rounded-lg shadow-md overflow-hidden">
+            <img src="/google_fotos.avif" alt="Descrição da imagem 1" className="w-1/3 object-cover"/>
+            <div className="p-4">
+              <h3 className="text-lg font-medium mb-2">Google Fotos: 6 novos recursos de IA que você precisa testar agora no app</h3>
+              <p className="text-base text-gray-700 line-clamp-3">
+                Google Fotos liberou ferramentas de IA para uso gratuito a todos os usuários da plataforma; veja como fazer edições usando recursos como borracha mágica e modo retrato
+              </p>
+              <a href="#" className="text-blue-500 hover:text-blue-700">Leia Mais</a>
+            </div>
+          </article>
+          <article className="flex rounded-lg shadow-md overflow-hidden">
+            <img src="/headset.avif" alt="Descrição da imagem 1" className="w-1/3 object-cover"/>
+            <div className="p-4">
+              <h3 className="text-lg font-medium mb-2">Headset gamer Redragon: confira 6 fones bons e baratos para jogar</h3>
+              <p className="text-base text-gray-700 line-clamp-3">
+              Redragon Zeus, Hero, Lamia e Diomedes são alguns dos modelos da marca focada em produtos para jogadores; fones com ou sem fio custam a partir de R$ 97 no varejo online
+              </p>
+              <a href="#" className="text-blue-500 hover:text-blue-700">Leia Mais</a>
+            </div>
+          </article>
+          <article className="flex rounded-lg shadow-md overflow-hidden">
+            <img src="/android_15.avif" alt="Descrição da imagem 1" className="w-1/3 object-cover"/>
+            <div className="p-4">
+              <h3 className="text-lg font-medium mb-2">Android 15 pode aumentar a duração da bateria em até 3 horas; entenda2</h3>
+              <p className="text-base text-gray-700 line-clamp-3">
+              Android 15 terá recurso que irá ajudar a gerir a bateria e melhorar a vida útil, com possibilidade de aumentar em até 3 horas o funcionamento; confira detalhes
+              </p>
+              <a href="#" className="text-blue-500 hover:text-blue-700">Leia Mais</a>
+            </div>
+          </article>
+        </div>
+      </section>
     </main>
   );
 }
