e8144a8773530784d6b31f8bbde0dc24fad9ba65
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-21 11:48:22 -0300

    Adicionando os banners no caroussel de imagens

--- tech_news/src/app/components/CarouselNews.tsx
+++ tech_news/src/app/components/CarouselNews.tsx
@@ -7,11 +7,9 @@ const CarouselNews = () => {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 mt-3 p-3">
       <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <img src="/ciberseguranca.webp" alt="..." />
+        <img src="/IA_webinar.webp" alt="..." />
+        <img src="/neuralink.jpg" alt="..." />
       </Carousel>
     </div>
   );
