8a4c59e5e09b7bef7ca824bd8b4fe5d65b59fc40
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-25 08:47:14 -0300

    Alterando a cor de fundo da Navbar

--- tech_news/src/app/components/NavBar.tsx
+++ tech_news/src/app/components/NavBar.tsx
@@ -17,7 +17,7 @@ function classNames(...classes) {
 
 const NavBar = ({ isDarkMode, toggleDarkMode }) => {
   return (
-    <Disclosure as="nav" className="bg-gray-700">
+    <Disclosure as="nav" className="bg-purple-900">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
