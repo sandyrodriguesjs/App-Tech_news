5500acc0413306c259c068e8940c6ea238b7484c
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-26 17:25:41 -0300

    Adicionando os links das redes sociais no Footer

--- tech_news/src/app/components/Footer.tsx
+++ tech_news/src/app/components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
                         <img src="/whatsapp.png" alt="" width="25" />
                     </div>
                     <div className='mx-2 hover:text-blue-500'>
-                        <h2><Link href=''>Watsapp</Link></h2>
+                        <h2><Link href='https://web.whatsapp.com'>Watsapp</Link></h2>
                     </div>
                 </div>
                 <div className='flex items-center mx-4'>
@@ -19,7 +19,7 @@ const Footer = () => {
                         <img src="/email.png" alt="" width="25" />
                     </div>
                     <div className='mx-2 hover:text-blue-500'>
-                    <h2><Link href=''>Email</Link></h2>
+                    <h2><Link href='mailto:mikaelantonio398@gmail.com'>Email</Link></h2>
                     </div>
                 </div>
                 <div className='flex items-center mx-4'>
@@ -27,7 +27,7 @@ const Footer = () => {
                         <img src="/instagram.png" alt="" width="25" />
                     </div>
                     <div className='mx-2 hover:text-blue-500'>
-                        <h2><Link href=''>Instagram</Link></h2>
+                        <h2><Link href='https://www.instagram.com'>Instagram</Link></h2>
                     </div>
                 </div>
             </div>
