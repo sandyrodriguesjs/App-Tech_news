d5fefa783adecfd437f4c87521909ff2e0e8a3ec
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-26 08:32:03 -0300

    Adicionando novas estilizações para que o footer sempre esteja no rodapé da página independente da situação

--- tech_news/src/app/layout.tsx
+++ tech_news/src/app/layout.tsx
@@ -32,11 +32,13 @@ export default function RootLayout({
     setIsDarkMode(!isDarkMode);
   };
   return (
-    <html lang="en">
-      <body className={`${inter.className} ${isDarkMode ? "bg-gray-900" : "bg-white"}`}>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} ${isDarkMode ? "bg-gray-900" : "bg-white"} flex flex-col min-h-full`}>
         <NavBar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-        {children}
-        <footer className='bg-gray-800 text-white py-4 mt-10 my-auto'>
+        <main className="flex-grow">
+          {children}
+        </main>
+        <footer className='bg-gray-800 text-white py-4 mt-10 my-auto position:fixed'>
           <Footer />
         </footer>
       </body>
