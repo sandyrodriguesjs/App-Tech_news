bba8fa05b3aad402de88cae93a052ed37276a346
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-26 08:56:14 -0300

    Aplicando novas estilizações para que as imagens do caroussel sejam adaptadas ao modo de tela mobile

--- tech_news/src/app/components/CarouselNews.tsx
+++ tech_news/src/app/components/CarouselNews.tsx
@@ -6,12 +6,12 @@ import { Carousel } from "flowbite-react";
 const CarouselNews = () => {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 mt-3 p-3">
-      <Carousel>
-        <img src="/ciberseguranca.webp" alt="..." />
-        <img src="/IA_webinar.webp" alt="..." />
-        <img src="/neuralink.jpg" alt="..." />
-      </Carousel>
-    </div>
+    <Carousel>
+      <img src="/ciberseguranca.webp" alt="Imagem sobre cibersegurança" className="w-full h-full object-cover" />
+      <img src="/IA_webinar.webp" alt="Imagem sobre IA webinar" className="w-full h-full object-cover" />
+      <img src="/neuralink.jpg" alt="Imagem sobre Neuralink" className="w-full h-full object-cover" />
+    </Carousel>
+  </div>
   );
 }
 
