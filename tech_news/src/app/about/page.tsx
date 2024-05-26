e0e6dbc631006fb1686a031d331cb41799a9dca3
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-26 09:52:22 -0300

    Criando a pagaina de about do site, onde terá informações detalhadas sobre o projeto

---
+++ tech_news/src/app/about/page.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return(
+        <div className="max-w-3xl mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-3">
+            <div className="p-3">
+                <p className="text-gray-900 text-justify">Bem-vindo ao Tech News Hub, o seu portal de notícias inovador! Nossa missão é mantê-lo informado com as principais notícias do dia, proporcionando uma leitura agradável e envolvente.</p>
+                <ul className="list-disc list-inside mt-4 text-gray-700 text-justify">
+                    <li>Notícias de Destaque: Nossa landing page exibe as principais notícias de forma clara e acessível, permitindo que você esteja sempre atualizado com os acontecimentos mais importantes.</li>
+                    <li>Artigos Detalhados: Aprofunde-se nos temas que mais lhe interessam com nossos artigos completos e bem elaborados, oferecendo análises e informações adicionais sobre as notícias.</li>
+                    <li>Personalização de Tema: Sabemos que cada usuário tem suas preferências. Por isso, oferecemos a opção de personalizar o tema do site, podendo escolher entre os modos dark e light para uma experiência visual mais confortável.</li>
+                    <li>Design Intuitivo: O design do nosso site foi criado pensando na melhor experiência para você. Com uma aparência estética apurada e uma navegação intuitiva, garantimos que encontrar e ler notícias seja uma experiência agradável.</li>
+                    <li>Adaptabilidade Mobile: Em um mundo onde estamos sempre em movimento, nosso site é completamente adaptável para dispositivos móveis. Seja no seu smartphone ou tablet, você terá a mesma qualidade de leitura e usabilidade que teria em um desktop.</li>
+                </ul>
+                <p className="mt-4 text-gray-900 text-justify">Estamos comprometidos em oferecer um conteúdo de qualidade com a melhor experiência possível para nossos usuários. Explore, personalize e fique sempre bem informado com o Tech News Hub.</p>
+            </div>
+        </div>
+    )
+}
